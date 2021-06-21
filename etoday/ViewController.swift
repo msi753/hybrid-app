6b59ec24bb8b86b91c0ed704bbdbc5dd47c103ed
msi753 <msi753@naver.com>
2021-06-21 17:08:51 +0900

    앱에서 자바스크립트 코드 실행시키고 공유기능 추가하기

--- etoday/ViewController.swift
+++ etoday/ViewController.swift
@@ -19,9 +19,7 @@ class ViewController: UIViewController {
     
     let defaultParam: String = "utm_device=IOS"
     
-    var navigationUrlString: String = ""
-    
-    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet var webView: WKWebView!
     
     @IBOutlet weak var backButton: UIBarButtonItem!
     @IBOutlet weak var forwardButton: UIBarButtonItem!
@@ -30,26 +28,34 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("viewDidLoad 호출")
+    override func loadView() {
+        print("loadView")
         
-        webView.uiDelegate = self
+        let contentController = WKUserContentController()
+        contentController.add(self, name: "Test")
+        
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        
+        webView = WKWebView(frame: .zero, configuration: config)
+        view = webView
+        
         webView.navigationDelegate = self
         
-        // 네트워크가 unreachable이 되면 OfflineVC를 보여준다
         NetworkManager.isUnreachable { _ in
             self.showOfflinePage()
         }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
         
-//        NetworkManager.isReachable { _ in
-//            self.showMainPage()
-//        }
-        
-        webView.configuration.userContentController.add(self, name: "showSnsShare_IOS()")
-        
-        //initWebViewThenCallFromJs()
-        loadUrl(urlString)
+        let url = URL(string: urlString)
+        if let myUrl = url {
+            let req = URLRequest(url: myUrl)
+            webView.load(req)
+        }
     }
     
     private func showOfflinePage() -> Void {
@@ -58,28 +64,10 @@ class ViewController: UIViewController {
         }
     }
     
-//    private func showMainPage() -> Void {
-//        DispatchQueue.main.async {
-//            self.performSegue(withIdentifier: "MainController", sender: self)
-//        }
-//    }
-    
-
-    func loadUrl(_ urlString: String) {
-        //view.addSubview(self.webView)
-        
-        print("\(urlString): loadURL메소드 호출")
-        
-        let url = URL(string: urlString)
-        if let myUrl = url {
-            let req = URLRequest(url: myUrl)
-            self.webView.load(req)
-        }
-    }
-    
     @IBAction func unwindToVC(_ segue: UIStoryboardSegue) {
            
     }
+    
     // MARK: - 툴바
     @IBAction func touchBack(_ sender: Any) {
         if self.webView.canGoBack {
@@ -94,13 +82,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchHome(_ sender: Any) {
-//        let url = URL(string: self.urlString)
-//        guard let myUrl = url else {
-//            return
-//        }
-//        let req = URLRequest(url: myUrl)
-//        self.webView.load(req)
-        loadUrl(urlString)
+        //
     }
     
     @IBAction func touchRefresh(_ sender: Any) {
@@ -108,106 +90,20 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchStop(_ sender: Any) {
-        //마지막 기능을 뭘로 해야할지 모르겠다...
-        //검색? 이 페이지 공유하기? 설정? 확대?
-        self.webView.stopLoading()
     }
 }
-
-// MARK: - WKUIDelegate
-
-extension ViewController: WKUIDelegate {
-    //toolbar IBOutlet으로 선언하고
-    //load가 시작되면 refresh를 stop으로
-    //load가 끝나면 stop을 refresh로?
-    //completion Handler를 호출하지 않으면 자바스크립트의 alert가 blocking하기 때문이다
-//    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
-//        let ac = UIAlertController(title: "Hey, listen!", message: message, preferredStyle: .alert)
-//        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-//        present(ac, animated: true)
-//        completionHandler()
-//    }
-//
-//    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
-//        let alert = UIAlertController(title: "", message: message, preferredStyle: .alert)
-//        let cancelAction = UIAlertAction(title: "CANCEL", style: .cancel, handler: {(action) in completionHandler(false)})
-//        let okAction = UIAlertAction(title: "OK", style: .default, handler: {(action) in completionHandler(true)})
-//        alert.addAction(cancelAction)
-//        alert.addAction(okAction)
-//        self.present(alert, animated: true, completion: nil)
-//    }
-}
-
 
 // MARK: - WKNavigationDelegate
 
 extension ViewController: WKNavigationDelegate {
-
-    func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
-        print("중복 리로드 방지")
-    }
-    
-    
-    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-        print("스피너 돌기 시작")
-        self.spinner.startAnimating()
-    }
-    
-    //웹뷰 로드 끝
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print("didFinish")
-        self.spinner.stopAnimating()
-        
-    }
-    
-    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-        self.spinner.stopAnimating()
-        print(" didFail 상세 페이지를 읽어오지 못했습니다")
-    }
-    
-    //href="_blank" 처리
-    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
-        if navigationAction.targetFrame == nil {
-            webView.load(navigationAction.request)
-        }
-        return nil
-        
-    }
-    
-    func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
-        print("리다이렉트")
-        print("serverRedirect")
-    }
-    
-    //여기가 스피너 돌려야하는 곳 아니여? 아니야^^
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        
-        print("start")
-    }
-    
-    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
-        decisionHandler(.allow)
-        print("캔슬이 사용됨")
-    }
-    
-    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-        self.spinner.stopAnimating()
-        print(" didFailProvisionalNavigation 상세 페이지를 읽어오지 못했습니다")
-    }
-    
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-//        for page in webView.backForwardList.backList {
-//            print("User visited \(page.url.absoluteString)")
-//        }
         
         guard let url = navigationAction.request.url else {
-            print(5)
             decisionHandler(.cancel)
             return
         }
         
         guard let host = url.host else {
-            print(6)
             decisionHandler(.cancel)
             return
         }
@@ -227,30 +123,41 @@ extension ViewController: WKNavigationDelegate {
         
         //처음에 param이 없을 때만 이동을 허용한다
         if !urlAbsoluteString.contains("utm_device=IOS") {
-            //print(1)
             if host.contains("dev.etoday.loc") {
-                //print(2)
                 var urlString: String = ""
                 if !urlAbsoluteString.contains("?") {
                     urlString += "?" + defaultParam
                 } else {
                     urlString += "&" + defaultParam
                 }
-                self.loadUrl(urlAbsoluteString + urlString)
-                //decisionHandler(.allow)
-                //return
+                
+                let url = URL(string: urlAbsoluteString+urlString)
+                if let myUrl = url {
+                    let req = URLRequest(url: myUrl)
+                    print(req)
+                    webView.load(req)
+                }
+                
             }
         }
         
-        //print(3)
         decisionHandler(.allow)
     }
 }
 
+// MARK: - WKScriptMessageHandler
 extension ViewController: WKScriptMessageHandler {
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        print(message)
+        if message.name == "Test" {
+            let values: [String:String] = message.body as! Dictionary
+            if let idx = values["idx"], let title = values["title"] {
+                if let appName = Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String {
+                    let linkUrl = urlString+"/view.php?idxno="+idx
+                    let textToShare = [ "[\(appName)] \(title) - \(linkUrl)" ]
+                    let activityVC = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+                    self.present(activityVC, animated: true, completion: nil)
+                }
+            }
+        }
     }
-    
-    
 }
